93581a58429daa8fbba37fe7ceab3905298c6530
Jez-er <103408350+Jez-er@users.noreply.github.com>
2026-04-13 13:21:29 +0300

    [UPDATE] Rename page files

--- src/app/router/admin.router.ts
+++ src/app/router/admin.router.ts
@@ -1,17 +1,17 @@
-import DashboardPage from "@/pages/admin/dashboard/DashboardPage.vue";
-import CategoryPage from "@/pages/admin/categories/CategoryPage.vue";
-import CategoryAddPage from "@/pages/admin/categories/add/CategoryAddPage.vue";
-import LanguagesPage from "@/pages/admin/languages/LanguagesPage.vue";
-import LanguageCreatePage from "@/pages/admin/languages/create/LanguageCreatePage.vue";
-import LanguageLevelsPage from "@/pages/admin/languages/levels/LanguageLevelsPage.vue";
-import LanguageLevelCreatePage from "@/pages/admin/languages/levels/create/LanguageLevelCreatePage.vue";
+import DashboardAdminPage from "@/pages/admin/dashboard/DashboardAdminPage.vue";
+import CategoryAdminPage from "@/pages/admin/categories/CategoryAdminPage.vue";
+import CategoryAddAdminPage from "@/pages/admin/categories/add/CategoryAddAdminPage.vue";
+import LanguagesAdminPage from "@/pages/admin/languages/LanguagesAdminPage.vue";
+import LanguageCreateAdminPage from "@/pages/admin/languages/create/LanguageCreateAdminPage.vue";
+import LanguageLevelsAdminPage from "@/pages/admin/languages/levels/LanguageLevelsAdminPage.vue";
+import LanguageLevelCreateAdminPage from "@/pages/admin/languages/levels/create/LanguageLevelCreateAdminPage.vue";
 
 export const adminRoutes = [
-  { path: "dashboard", component: DashboardPage },
-  { path: "categories", component: CategoryPage },
-  { path: "categories/create", component: CategoryAddPage },
-  { path: "languages", component: LanguagesPage },
-  { path: "languages/create", component: LanguageCreatePage },
-  { path: "languages/levels", component: LanguageLevelsPage },
-  { path: "languages/levels/create", component: LanguageLevelCreatePage },
+  { path: "dashboard", component: DashboardAdminPage },
+  { path: "categories", component: CategoryAdminPage },
+  { path: "categories/create", component: CategoryAddAdminPage },
+  { path: "languages", component: LanguagesAdminPage },
+  { path: "languages/create", component: LanguageCreateAdminPage },
+  { path: "languages/levels", component: LanguageLevelsAdminPage },
+  { path: "languages/levels/create", component: LanguageLevelCreateAdminPage },
 ];
