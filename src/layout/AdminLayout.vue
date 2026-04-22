71ae4e788a58823ee459a9fde8a2239ea474be3f
Jez-er <103408350+Jez-er@users.noreply.github.com>
2026-04-22 18:33:37 +0300

    add header in admin panel

--- src/layout/AdminLayout.vue
+++ src/layout/AdminLayout.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
-import Sidebar from "@/components/admin/SideBar/Sidebar.vue";
+import Sidebar from '@/components/admin/SideBar/Sidebar.vue'
+import ThemeToggle from '@/shared/ui/theme-toggle/ThemeToggle.vue'
 </script>
 
 <template>
-  <main class="flex gap-2 h-svh overflow-hidden">
-    <Sidebar />
-    <div class="flex-1 overflow-y-auto">
-      <router-view />
-    </div>
-  </main>
+	<main class="flex h-svh overflow-hidden">
+		<Sidebar />
+		<div class="flex-1">
+			<header class="h-16 border-b-2 flex items-center justify-between px-4">
+				<div>Admin Panel</div>
+				<ThemeToggle />
+			</header>
+			<main class="p-10 h-screen w-full overflow-auto">
+				<router-view />
+			</main>
+		</div>
+	</main>
 </template>
